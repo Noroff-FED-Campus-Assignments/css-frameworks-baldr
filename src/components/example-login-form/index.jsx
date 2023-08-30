6cd78896a3cc7977a193f5f71e5271144c3bd0ae
Haakonjohansenn <111745800+Haakonjohansenn@users.noreply.github.com>
2023-08-30 12:15:49 +0200

    changed login images to flex-box

--- src/components/example-login-form/index.jsx
+++ src/components/example-login-form/index.jsx
@@ -38,109 +38,103 @@ function ExampleLoginForm() {
           </button>
         </>
       )}
-      <div className="relative overflow-hidden bg-white">
-      <div className="pb-80 pt-16 sm:pb-40 sm:pt-24 lg:pb-48 lg:pt-40">
-        <div className="relative mx-auto max-w-7xl px-4 sm:static sm:px-6 lg:px-8">
-          <div className="sm:max-w-lg">
-            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-              Summer styles are finally here
-            </h1>
-            <p className="mt-4 text-xl text-gray-500">
-              This year, our new summer collection will shelter you from the harsh elements of a world that does not care
-              if you live or die.
-            </p>
-          </div>
-          <div>
-            <div className="mt-10">
-
-              
-
-
-              {/* Decorative image grid */}
-              <div
-                aria-hidden="true"
-                className="pointer-events-none lg:absolute lg:inset-y-0 lg:mx-auto lg:w-full lg:max-w-7xl"
-              >
-                <div className="absolute transform sm:left-1/2 sm:top-0 sm:translate-x-8 lg:left-1/2 lg:top-1/2 lg:-translate-y-1/2 lg:translate-x-8">
-                  <div className="flex items-center space-x-6 lg:space-x-8">
-                    <div className="grid flex-shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8">
-                      <div className="h-64 w-44 overflow-hidden rounded-lg sm:opacity-0 lg:opacity-100">
-                        <img
-                          src="/dog1.jpg"
-                          alt=""
-                          className="h-full w-full object-cover object-center"
-                        />
-                      </div>
-                      <div className="h-64 w-44 overflow-hidden rounded-lg">
-                        <img
-                          src="/dog2.jpg"
-                          alt=""
-                          className="h-full w-full object-cover object-center"
-                        />
-                      </div>
-                    </div>
-                    <div className="grid flex-shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8">
-                      <div className="h-64 w-44 overflow-hidden rounded-lg">
-                        <img
-                          src="/dog3.jpg"
-                          alt=""
-                          className="h-full w-full object-cover object-center"
-                        />
-                      </div>
-                      <div className="h-64 w-44 overflow-hidden rounded-lg">
-                        <img
-                          src="/dog4.jpg"
-                          alt=""
-                          className="h-full w-full object-cover object-center"
-                        />
-                      </div>
-                      <div className="h-64 w-44 overflow-hidden rounded-lg">
-                        <img
-                          src="/dog5.jpg"
-                          alt=""
-                          className="h-full w-full object-cover object-center"
-                        />
-                      </div>
-                    </div>
-                    <div className="grid flex-shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8">
-                      <div className="h-64 w-44 overflow-hidden rounded-lg">
-                        <img
-                          src="/dog6.jpg"
-                          alt=""
-                          className="h-full w-full object-cover object-center"
-                        />
-                      </div>
-                      <div className="h-64 w-44 overflow-hidden rounded-lg">
-                        <img
-                          src="/dog7.jpg"
-                          alt=""
-                          className="h-full w-full object-cover object-center"
-                        />
-                      </div>
-                    </div>
-                  </div>
-                </div>
+<div className="relative overflow-hidden bg-white">
+  <div className="pb-80 pt-16 sm:pb-40 sm:pt-24 lg:pb-48 lg:pt-40">
+    <div className="relative mx-auto max-w-7xl px-4 sm:static sm:px-6 lg:px-8">
+      <div className="sm:max-w-lg">
+        <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+          Summer styles are finally here
+        </h1>
+        <p className="mt-4 text-xl text-gray-500">
+          This year, our new summer collection will shelter you from the harsh elements of a world that does not care if you live or die.
+        </p>
+      </div>
+      <div>
+        <div className="mt-10">
+          {/* Decorative image grid */}
+          <div className="flex flex-wrap -mx-4">
+            <div className="w-full sm:w-1/2 lg:w-1/3 px-4 mb-6">
+              <div className="h-64 overflow-hidden rounded-lg">
+                <img
+                  src="/dog1.jpg"
+                  alt=""
+                  className="h-full w-full object-cover object-center"
+                />
               </div>
-
-              <a
-                href="#"
-                className="inline-block rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-center font-medium text-white hover:bg-indigo-700"
-              >
-                Sign in
-              </a>
-              
+            </div>
+            <div className="w-full sm:w-1/2 lg:w-1/3 px-4 mb-6">
+              <div className="h-64 overflow-hidden rounded-lg">
+                <img
+                  src="/dog2.jpg"
+                  alt=""
+                  className="h-full w-full object-cover object-center"
+                />
+              </div>
+            </div>
+            <div className="w-full sm:w-1/2 lg:w-1/3 px-4 mb-6">
+              <div className="h-64 overflow-hidden rounded-lg">
+                <img
+                  src="/dog3.jpg"
+                  alt=""
+                  className="h-full w-full object-cover object-center"
+                />
+              </div>
+            </div>
+            <div className="w-full sm:w-1/2 lg:w-1/3 px-4 mb-6">
+              <div className="h-64 overflow-hidden rounded-lg">
+                <img
+                  src="/dog4.jpg"
+                  alt=""
+                  className="h-full w-full object-cover object-center"
+                />
+              </div>
+            </div>
+            <div className="w-full sm:w-1/2 lg:w-1/3 px-4 mb-6">
+              <div className="h-64 overflow-hidden rounded-lg">
+                <img
+                  src="/dog5.jpg"
+                  alt=""
+                  className="h-full w-full object-cover object-center"
+                />
+              </div>
+            </div>
+            <div className="w-full sm:w-1/2 lg:w-1/3 px-4 mb-6">
+              <div className="h-64 overflow-hidden rounded-lg">
+                <img
+                  src="/dog6.jpg"
+                  alt=""
+                  className="h-full w-full object-cover object-center"
+                />
+              </div>
+            </div>
+            <div className="w-full sm:w-1/2 lg:w-1/3 px-4 mb-6">
+              <div className="h-64 overflow-hidden rounded-lg">
+                <img
+                  src="/dog7.jpg"
+                  alt=""
+                  className="h-full w-full object-cover object-center"
+                />
+              </div>
             </div>
           </div>
+          <a
+            href="#"
+            className="inline-block rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-center font-medium text-white hover:bg-indigo-700"
+          >
+            Sign in
+          </a>
         </div>
       </div>
     </div>
+  </div>
+</div>
 
-    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            Sign in to your account
-          </h2>
-        </div>
+<div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+  <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+    <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+      Sign in to your account
+    </h2>
+  </div>
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
           <form className="space-y-6" action="#" method="POST">
