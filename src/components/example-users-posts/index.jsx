d16bab7191ff7a6668e8903be1390e5561dad5dd
juliakalvik <julia.kalvik@yahoo.no>
2023-08-30 12:22:28 +0200

    Added to API data

--- src/components/example-users-posts/index.jsx
+++ src/components/example-users-posts/index.jsx
@@ -1,18 +1,25 @@
-import { useUsersPosts } from "../../lib/my-api";
+
 
 const listOfPosts = [
   {
     id: 1,
-    title: "Post 1",
-    userName: "Dog 1",
-    image: "/dog1.jpg"
+    profileImage: "/dog1.jpg",
+    userName: "Balder",
+    date: "10 minutes ago",
+    title: "Felt cute",
+    contentText: "lorem ipsuhuuuuhuuuuuum",
+    contentImage: "/dog1post.webp"
+
   },
   {
     id: 2,
-    title: "Post 2",
-    userName: "Dog 2",
-    image: "/dog2.jpg"
-  }
+    profileImage: "/dog2.jpg",
+    userName: "Melissa",
+    date: "11 minutes ago",
+    title: "Just chillin'",
+    contentText: "My human gave me a spa day and now I feel refreshed",
+    contentImage: "/dog2post.jpg"
+  },
 ];
 
 function ExampleUsersPosts() {
@@ -27,17 +34,71 @@ function ExampleUsersPosts() {
   if (error) {
     return <span>Error: {error.message}</span>;
   }
+
+import { useUsersPosts } from "../../lib/my-api";
+
 */
   return (
     <>
 
         {listOfPosts.map((post) => (
           <div key={post.id}>
-            <h3>{post.title}</h3>
+            <img src={post.profileImage} />
             <h3>{post.userName}</h3>
-            <img src={post.image} alt={post.title} />
+            <p>{post.date}</p>
+            <h3>{post.title}</h3>
+            <p>{post.contentText}</p>
+            <img src={post.contentImage} />
           </div>
         ))}
+        <div className="bg-white py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl lg:mx-0">
+          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">From the blog</h2>
+          <p className="mt-2 text-lg leading-8 text-gray-600">
+            Learn how to grow your business with our expert advice.
+          </p>
+        </div>
+        <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+          {listOfPosts.map((post) => (
+            <article key={post.id} className="flex max-w-xl flex-col items-start justify-between">
+              <div className="flex items-center gap-x-4 text-xs">
+                <time dateTime={post.datetime} className="text-gray-500">
+                  {post.date}
+                </time>
+                <a
+                  href={post.title}
+                  className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
+                >
+                  {post.category.title}
+                </a>
+              </div>
+              <div className="group relative">
+                <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+                  <a href={post.href}>
+                    <span className="absolute inset-0" />
+                    {post.title}
+                  </a>
+                </h3>
+                <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">{post.description}</p>
+              </div>
+              <div className="relative mt-8 flex items-center gap-x-4">
+                <img src={post.author.imageUrl} alt="" className="h-10 w-10 rounded-full bg-gray-50" />
+                <div className="text-sm leading-6">
+                  <p className="font-semibold text-gray-900">
+                    <a href={post.author.href}>
+                      <span className="absolute inset-0" />
+                      {post.author.name}
+                    </a>
+                  </p>
+                  <p className="text-gray-600">{post.author.role}</p>
+                </div>
+              </div>
+            </article>
+          ))}
+        </div>
+      </div>
+    </div>
     </>
   );
 }
