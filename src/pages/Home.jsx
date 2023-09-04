c03f8d9f5794b96d94f1675f81e0234acbcba18a
Haakonjohansenn <111745800+Haakonjohansenn@users.noreply.github.com>
2023-09-04 18:40:55 +0200

    made search bar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,16 @@
 import ExampleUsersPosts from "../components/example-users-posts";
 import CreatePostForm from "../components/example-create-post";
+import "../App.scss";
 
 function HomePage() {
   return (
     <>
+    <div className="flex justify-center">
+      <input className="p-1 border-solid" type="search" placeholder="Search..."/>
+      <button>
+      🔎
+      </button>
+    </div>
       <ExampleUsersPosts />
       <CreatePostForm />
     </>
